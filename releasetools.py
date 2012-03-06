4f6f17834cc847d37b6612b90bb9edb0078ee68a
gexudong <gexudong@xiaomi.com>
2012-03-06 20:56:12 +0800

    fix releasetools.py
    Change-Id: I308234936b34feb3fe3183ab17967e63bcba9c5b

--- releasetools.py
+++ releasetools.py
@@ -5,7 +5,7 @@ def AddAssertions(info):
     edify = info.script
     for i in xrange(len(edify.script)):
         if ");" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
-            edify.script[i] = edify.script[i].replace(");", ' || getprop("ro.product.device") == "galaxy2" || getprop("ro.build.product") == "galaxy2");')
+            edify.script[i] = edify.script[i].replace(");", ' || getprop("ro.product.device") == "galaxys2" || getprop("ro.build.product") == "galaxys2");')
             return
 
 def FullOTA_InstallEnd(info):
