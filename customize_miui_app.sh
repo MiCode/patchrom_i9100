1b37e890bbb5d9f1112493ead5af40275892dc33
gexudong <gexudong@xiaomi.com>
2012-03-12 12:02:04 +0800

    fix the crash issue when 2g/3g network switch
    Change-Id: I1829cd54f73437009b33c9a0c7d35050eb8b6218

---
+++ customize_miui_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# $1: dir for original miui app 
+# $2: dir for target miui app
+#
+
+XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
+
+if [ $1 = "MiuiSystemUI" ];then
+	$XMLMERGYTOOL $1/res/values $2/res/values
+fi
