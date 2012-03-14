5dec231e35af659e4a0f62a60b49c250b3470ef5
yumingyang <yumingyang@xiaomi.com>
2012-03-14 15:03:56 +0800

    add the new SamsungSpecialSettings and it's apk
    Change-Id: Idc1ed65c5faf40e9477ee0e1e54b50cd6db8ebb0

---
+++ SamsungSpecialSettings/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_JAVA_LIBRARIES := bouncycastle
+LOCAL_STATIC_JAVA_LIBRARIES := guava
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_PACKAGE_NAME := SamsungSettings
+LOCAL_CERTIFICATE := platform
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_PROGUARD_ENABLED := obfuscate
+
+include $(BUILD_PACKAGE)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ SamsungSpecialSettings/src/com/android/samsungspecialsettings/SamsungSpecialSettings.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.samsungspecialsettings;
+import android.app.ActivityManagerNative;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceActivity;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.view.IWindowManager;
+import android.view.Surface;
+import android.view.MenuItem;
+import android.app.ActionBar;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+
+import java.util.ArrayList;
+
+public class SamsungSpecialSettings extends PreferenceActivity implements
+    Preference.OnPreferenceChangeListener{
+    private static final String LOG_TAG = "SamsungSpecialSettingsLog";
+
+    /** If there is no setting in the provider, use this. */
+    private static final String KEY_SCREEN_MODE = "screen_mode";
+    private static final String KEY_TOUCH_KEY_LIGHT = "touch_key_light";
+    private static final String KEY_DISPLAY_SAVING = "display_saving";
+    private static final String KEY_A_SENSOR = "a_sensor";
+    private static final String KEY_G_SENSOR = "g_sensor";
+
+    private Preference mScreenMode;
+    private ListPreference mTouchKeyLight;
+    private CheckBoxPreference mDisplaySaving;
+    private Preference mAccSensor;
+    private Preference mGyroSensor;
+ 
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(LOG_TAG, "onCreate");
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+              actionBar.setHomeButtonEnabled(true);
+        }
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.samsung_special_settings);
+        mScreenMode = (Preference) findPreference(KEY_SCREEN_MODE);
+
+        mTouchKeyLight = (ListPreference) findPreference(KEY_TOUCH_KEY_LIGHT);
+        //please read the value of TouchKeyLightDuration and set it to mTouchKeyLight
+        //mTouchKeyLight.setValue(String.valueOf(currentTimeout));
+        mTouchKeyLight.setOnPreferenceChangeListener(this);
+        mTouchKeyLight.setEnabled(true);
+        
+        mDisplaySaving = (CheckBoxPreference) findPreference(KEY_DISPLAY_SAVING);
+        updateAutoAdjustScreenPowerState();
+
+        mAccSensor = (Preference) findPreference(KEY_A_SENSOR);
+        mGyroSensor = (Preference) findPreference(KEY_G_SENSOR);
+    }
+    
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mScreenMode) {
+            Log.i(LOG_TAG, "Screen mode");
+            try {
+                //please start your Activity
+                startActivity(new Intent("android.intent.action.TEST", Uri.parse("content://test/example")));
+            } catch (ActivityNotFoundException e) {
+                Log.w(LOG_TAG, "No Activity found to handle Intent :", e);
+            }
+        }else if (preference == mTouchKeyLight){
+            Log.i(LOG_TAG, "Touch key light duration");
+        }else if(preference == mDisplaySaving){
+            Log.i(LOG_TAG, "Auto adjust screen power");
+            if (mDisplaySaving.isChecked()) {
+                Log.i(LOG_TAG, "Checked");
+                //please write the value in to the database here
+            } else {
+                Log.i(LOG_TAG, "Unchecked");
+                //please write the value in to the database here
+            }
+        }else if(preference == mAccSensor){
+            Log.i(LOG_TAG, "Horizontal Calibration");
+            try {
+                //please start your Activity
+                startActivity(new Intent("android.intent.action.TEST", Uri.parse("content://test/example")));
+            } catch (ActivityNotFoundException e) {
+                Log.w(LOG_TAG, "No Activity found to handle Intent :", e);
+            }
+        }else if(preference == mGyroSensor){
+            Log.i(LOG_TAG, "Gyroscope Calibration");
+            try {
+                //please start your Activity
+                startActivity(new Intent("android.intent.action.TEST", Uri.parse("content://test/example")));
+            } catch (ActivityNotFoundException e) {
+                Log.w(LOG_TAG, "No Activity found to handle Intent :", e);
+            }
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(LOG_TAG, "onOptionsItemSelected");
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    private void updateAutoAdjustScreenPowerState(){
+        Log.i(LOG_TAG, "updateAutoAdjustScreenPowerState");
+        //please update the state of mDisplaySaving here
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        Log.i(LOG_TAG, "onPreferenceChange");
+        final String key = preference.getKey();
+        if (KEY_TOUCH_KEY_LIGHT.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            //please put the value in to the database
+            Log.i(LOG_TAG, "value: " + value);
+        }
+        return true;
+    }
+}
+
