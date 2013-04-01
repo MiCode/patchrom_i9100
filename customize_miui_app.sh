f3bdd0daa402d98d9c6204f6a6956a604e13405b
mochangming <mochangming@xiaomi.com>
2013-04-01 10:30:41 +0800

    fix abstract method not implemented
    Change-Id: Id1b0b1122bf308ff18021a96ecf36a786d686b53

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -6,6 +6,16 @@
 
 XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
 
+function appendPart() {
+    for file in `find $1/smali -name *.part`
+    do
+		filepath=`dirname $file`
+		filename=`basename $file .part`
+		dstfile="out/$filepath/$filename"
+        cat $file >> $dstfile
+    done
+}
+
 if [ $1 = "Settings" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
 fi
@@ -24,4 +34,8 @@ fi
 
 if [ $1 = "MiuiHome" ];then
     $XMLMERGYTOOL $1/res/values-xhdpi $2/res/values-xhdpi
+fi
+
+if [ $1 = "MiuiSystemUI" ];then
+	appendPart $1
 fi
